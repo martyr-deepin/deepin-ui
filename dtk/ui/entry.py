e0af8c4e17628ce4ac1b3e3e2370287d0a0ad439
Andy Stewart <lazycat.manatee@gmail.com>
2012-03-29 09:24:56 +0800

    Update entry module.

--- dtk/ui/entry.py
+++ dtk/ui/entry.py
@@ -44,6 +44,7 @@ class Entry(gtk.EventBox):
         self.font_size = font_size
         self.content = content
         self.cursor_index = 0
+        self.offset_x = 0
         self.text_color = text_color
         self.text_select_color = text_select_color
         self.background_color = background_color
@@ -128,10 +129,8 @@ class Entry(gtk.EventBox):
             (text_width, text_height) = layout.get_pixel_size()
             
             # Move text.
-            if text_width < draw_width:
-                cr.move_to(draw_x, draw_y + (draw_height - text_height) / 2)
-            else:
-                cr.move_to(draw_x + draw_width - text_width, draw_y + (draw_height - text_height) / 2)
+            cr.move_to(draw_x - self.offset_x, 
+                       draw_y + (draw_height - text_height) / 2)
     
             # Draw text.
             cr.set_source_rgb(*color_hex_to_cairo(self.text_color.get_color()))
@@ -140,16 +139,41 @@ class Entry(gtk.EventBox):
             
     def draw_entry_cursor(self, cr, rect):
         '''Draw entry cursor.'''
-        pass
+        # Init.
+        x, y, w, h = rect.x, rect.y, rect.width, rect.height
+        left_str = self.content[0:self.cursor_index]
+        right_str = self.content[self.cursor_index::]
+        (left_str_width, left_str_height) = get_content_size(left_str, self.font_size)
+        
+        # Draw cursor.
+        cr.set_source_rgb(*color_hex_to_cairo(ui_theme.get_color("entryCursor").get_color()))
+        cr.rectangle(x + self.padding_x + left_str_width - self.offset_x,
+                     y + self.padding_y,
+                     1, 
+                     h - self.padding_y * 2)
+        cr.fill()
     
     def button_press_entry(self, widget, event):
         '''Button press entry.'''
         self.grab_focus()
         
-    def commit_entry(self, im, im_str):
+    def commit_entry(self, im, input_text):
         '''Entry commit.'''
-        self.content += im_str
-        self.cursor_index += len(im_str)
+        self.content = self.content[0:self.cursor_index] + input_text + self.content[self.cursor_index::]
+        self.cursor_index += len(input_text)
+        
+        (text_width, text_height) = get_content_size(self.content, self.font_size)
+        rect = self.get_allocation()
+        if text_width <= rect.width - self.padding_x * 2:
+            self.offset_x = 0
+        elif self.cursor_index == len(self.content):
+            self.offset_x = text_width - (rect.width - self.padding_x * 2)
+        else:
+            (old_text_width, old_text_height) = get_content_size(self.content[0:self.cursor_index - len(input_text)])
+            (input_text_width, input_text_height) = get_content_size(input_text)
+            if old_text_width - self.offset_x + input_text_width > rect.width - self.padding_x * 2:
+                (new_text_width, new_text_height) = get_content_size(self.content[0:self.cursor_index])
+                self.offset_x = new_text_width - (rect.width - self.padding_x * 2)
         
         self.queue_draw()
         
