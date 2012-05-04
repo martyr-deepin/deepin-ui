ef79e1dba74da94b72677ea08d2b000cce762bd8
Andy Stewart <lazycat.manatee@gmail.com>
2012-05-04 10:23:19 +0800

    Finish dominant_color.py

--- dtk/ui/dominant_color.py
+++ dtk/ui/dominant_color.py
@@ -21,7 +21,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from PIL import Image
-from draw import draw_pixbuf
+from draw import draw_pixbuf, draw_vlinear, draw_hlinear
 from utils import propagate_expose, color_hex_to_cairo
 import gtk
 import scipy
@@ -60,6 +60,7 @@ class ColorTestWidget(gtk.DrawingArea):
         self.connect("expose-event", self.color_test_widget_expose)        
         self.pixbuf = None
         self.background_color = "#000000"
+        self.buffer_size = 200       # pixel
         
         self.drag_dest_set(
             gtk.DEST_DEFAULT_MOTION |
@@ -74,24 +75,35 @@ class ColorTestWidget(gtk.DrawingArea):
         self.set_pixbuf(urllib.unquote(selection_data.get_uris()[0].split("file://")[1]))        
         
     def set_pixbuf(self, filename):
-        self.pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(filename, 400, 400)
+        self.pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(filename, 800, 800)
         self.background_color = get_dominant_color(filename)
-        print self.background_color
         self.queue_draw()
         
     def color_test_widget_expose(self, widget, event):
         cr = widget.window.cairo_create()
         rect = widget.allocation
         
+        # Draw pixbuf.
+        draw_pixbuf(cr, self.pixbuf, 0, 0)
+        
+        # Draw mask.
+        draw_vlinear(
+            cr, rect.x, rect.y + self.pixbuf.get_height() - self.buffer_size, rect.width, self.buffer_size,
+            [(0, (self.background_color, 0)),
+             (1, (self.background_color, 1))])
+        
+        draw_hlinear(
+            cr, rect.x + self.pixbuf.get_width() - self.buffer_size, rect.y, self.buffer_size, rect.height,
+            [(0, (self.background_color, 0)),
+             (1, (self.background_color, 1))])
+        
         # Draw background.
         cr.set_source_rgb(*color_hex_to_cairo(self.background_color))
-        cr.rectangle(0, 0, rect.width, rect.height)
+        cr.rectangle(rect.x + self.pixbuf.get_width(), rect.y,
+                     rect.width - self.pixbuf.get_width(), rect.height)
+        cr.rectangle(rect.x, rect.y + self.pixbuf.get_height(), 
+                     rect.width, rect.height - self.pixbuf.get_height())
         cr.fill()
-        
-        # Draw pixbuf.
-        draw_pixbuf(cr, self.pixbuf, 
-                    (rect.width - self.pixbuf.get_width()) / 2, 
-                    (rect.height - self.pixbuf.get_height()) / 2)
         
         propagate_expose(widget, event)
         
@@ -99,8 +111,9 @@ class ColorTestWidget(gtk.DrawingArea):
 
 if __name__ == '__main__':
     window = gtk.Window()
+    window.set_title("从文件管理器拖动图片测试主色")
     window.connect("destroy", lambda w: gtk.main_quit())
-    window.set_size_request(500, 500)
+    window.maximize()
     
     test = ColorTestWidget()
     test.set_pixbuf("/data/Picture/壁纸/1713311.jpg")
