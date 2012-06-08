1f5a77be4a37daf00d8c511ec0d7a261e202a6fe
Andy Stewart <lazycat.manatee@gmail.com>
2012-06-08 12:45:52 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -36,6 +36,7 @@ app_theme = Theme(
     os.path.join(get_parent_dir(__file__), "app_theme"),
     os.path.expanduser("~/.config/deepin-demo/theme"))
 app_theme.load_theme()
+skin_config.set_themes(ui_theme, app_theme)
 
 # Load other modules.
 from dtk.ui.application import Application
