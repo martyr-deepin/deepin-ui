227db1d5527419f9e3dd78e175cb92cc7977ef04
Andy Stewart <lazycat.manatee@gmail.com>
2012-08-07 08:04:35 +0800

    Create new module.

---
+++ dtk/ui/new_treeview.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011 ~ 2012 Deepin, Inc.
+#               2011 ~ 2012 Wang Yong
+# 
+# Author:     Wang Yong <lazycat.manatee@gmail.com>
+# Maintainer: Wang Yong <lazycat.manatee@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gtk
+import gobject
+from scrolled_window import ScrolledWindow
+
+class TreeView(ScrolledWindow):
+    '''
+    TreeView widget.
+    '''
+	
+    def __init__(self,
+                 items=[],
+                 sort_methods=[],
+                 row_normal_height=None,
+                 row_select_height=None,
+                 drag_data=None,
+                 enable_multiple_select=True,
+                 enable_drag_drop=True,
+                 drag_icon_pixbuf=None,
+                 start_drag_offset=50,
+                 right_space=2,
+                 top_bottom_space=3
+                 ):
+        '''
+        Initialize TreeView class.
+        '''
+        # Init.
+        ScrolledWindow.__init__(self, right_space, top_bottom_space)
+        self.draw_area = gtk.DrawingArea()
+        self.draw_align = gtk.Alignment()
+        self.draw_align.set(0.5, 0.5, 1, 1)
+        
+        self.draw_align.add(self.draw_area)
+        self.add_child(self.draw_align)
+
+gobject.type_register(TreeView)
+
+class TreeItem(gobject.GObject):
+    '''
+    Tree item template use for L{ I{TreeView} <TreeView>}.
+    '''
+	
+    def __init__(self):
+        '''
+        Initialize TreeItem class.
+        '''
+        pass
+        
+gobject.type_register(TreeItem)
