7e32387e1b5a78a585b5a94927b01867a22967e4
Andy Stewart <lazycat.manatee@gmail.com>
2012-09-07 01:04:16 +0800

    Finish progressbar expose method.

--- dtk/ui/progressbar.py
+++ dtk/ui/progressbar.py
@@ -20,13 +20,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from draw import draw_round_rectangle, draw_vlinear, draw_text, draw_radial_round
+from draw import draw_vlinear
 from theme import ui_theme
-from utils import alpha_color_hex_to_cairo, cairo_state, propagate_expose
+from utils import cairo_state, propagate_expose, color_hex_to_cairo, cairo_disable_antialias
 import cairo
 import gobject
 import gtk
-import pango
 
 class ProgressBar(gtk.Button):
     '''
@@ -43,12 +42,11 @@ class ProgressBar(gtk.Button):
         # Init.
         gtk.Button.__init__(self)
         self.progress = 0
-        self.light_ticker = 0
+        # self.progress = 100
         self.test_ticker = 0.0
         
         # Expose callback.
         self.connect("expose-event", self.expose_progressbar)
-        gtk.timeout_add(20, self.update_light_ticker)
         
     def expose_progressbar(self, widget, event):
         '''
@@ -57,50 +55,60 @@ class ProgressBar(gtk.Button):
         # Init.
         cr = widget.window.cairo_create()
         rect = widget.allocation
+        x, y, w, h = rect.x, rect.y, rect.width, rect.height
         
-        # Draw frame.
-        cr.set_source_rgba(*alpha_color_hex_to_cairo(ui_theme.get_alpha_color("progressbar_frame").get_color_info()))
-        cr.set_operator(cairo.OPERATOR_OVER)
-        draw_round_rectangle(cr, rect.x, rect.y, rect.width, rect.height, 1)
-        cr.stroke()
-        
-        # Draw background.
-        draw_vlinear(cr, rect.x, rect.y, rect.width, rect.height, 
-                     ui_theme.get_shadow_color("progressbar_background").get_color_info(), 
-                     1)
-    
-        # Draw foreground.
-        draw_vlinear(cr, rect.x, rect.y, rect.width * self.progress / 100.0, rect.height, 
-                     ui_theme.get_shadow_color("progressbar_foreground").get_color_info(), 
-                     1)
-        
-        # Draw font.
-        draw_text(cr, str(self.progress) + "%", 
-                  rect.x, rect.y, rect.width, rect.height, 
-                  rect.height - 5, "#000000",
-                  alignment=pango.ALIGN_CENTER)
-        
-        # Draw light.
-        light_radius = rect.height * 4
-        light_offset_x = min(self.light_ticker % 150, 100) / 100.0 * (rect.width + light_radius * 2)
+        # Draw background frame.
         with cairo_state(cr):
-            cr.rectangle(rect.x, rect.y, rect.width * self.progress / 100.0, rect.height)
+            cr.rectangle(x, y + 1, w, h - 2)
+            cr.rectangle(x + 1, y, w - 2, h)
             cr.clip()
-            draw_radial_round(cr, rect.x + light_offset_x - light_radius, rect.y - light_radius / 2, light_radius, 
-                              ui_theme.get_shadow_color("progressbar_light").get_color_info())
+            
+            cr.set_source_rgb(*color_hex_to_cairo("#b3b3b3"))
+            cr.rectangle(x, y, w, h)
+            cr.set_line_width(1)
+            cr.stroke()
+            
+        # Draw background.
+        with cairo_state(cr):
+            cr.rectangle(x + 1, y + 1, w - 2, h - 2)
+            cr.clip()
+            
+            draw_vlinear(cr, x + 1, y + 1, w - 2, h - 2,
+                         ui_theme.get_shadow_color("progressbar_background").get_color_info(), 
+                         )
+    
+        # Draw foreground frame.
+        with cairo_state(cr):
+            cr.rectangle(x, y + 1, w, h - 2)
+            cr.rectangle(x + 1, y, w - 2, h)
+            cr.clip()
+
+            cr.set_antialias(cairo.ANTIALIAS_NONE)
+            cr.set_source_rgb(*color_hex_to_cairo("#2e7599"))
+            cr.rectangle(x + 1, y + 1, int(w * self.progress / 100) - 1, h - 1)
+            cr.set_line_width(1)
+            cr.stroke()
+            
+        # Draw foreground.
+        with cairo_state(cr):
+            cr.rectangle(x + 1, y + 1, w - 2, h - 2)
+            cr.clip()
+            
+            draw_vlinear(cr, x + 1, y + 1, int(w * self.progress / 100) - 2, h - 2,
+                         ui_theme.get_shadow_color("progressbar_foreground").get_color_info(), 
+                         )
+            
+        # Draw light.
+        with cairo_disable_antialias(cr):
+            cr.set_source_rgba(1, 1, 1, 0.5)
+            cr.rectangle(x + 1, y + 1, w - 2, 1)
+            cr.fill()
                
         # Propagate expose.
         propagate_expose(widget, event)
         
         return True        
         
-    def update_light_ticker(self):
-        '''
-        Internal function to update light ticker.
-        '''
-        self.light_ticker += 1
-        return True
-            
     def test_progressbar(self):
         '''Test prorgressbar.'''
         self.test_ticker += 1
