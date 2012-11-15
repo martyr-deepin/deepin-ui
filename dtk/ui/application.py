d5193008fed606bc89756a88b75b41287247ed07
xiangzhai <xiangzhai83@gmail.com>
2012-11-15 15:51:20 +0800

    Set application title could be shown in status bar

--- dtk/ui/application.py
+++ dtk/ui/application.py
@@ -114,10 +114,10 @@ class Application(object):
 
         # Show titlebar.
         self.show_titlebar()
-
+        
         if app_name != None:
             self.window.set_title(app_name)
-        
+    
     def close_window(self, widget):
         """
         Close the window when the close button is clicked.
