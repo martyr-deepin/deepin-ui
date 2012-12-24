ed54806deb9b0bac81c2378aa2922f54bfd9a285
xiangzhai <xiangzhai83@gmail.com>
2012-12-24 17:41:57 +0800

    Init sexy DateTime widget

--- dtk/ui/datetime.py
+++ dtk/ui/datetime.py
@@ -28,6 +28,22 @@ import gtk
 from button import Button
 from label import Label
 from constant import ALIGN_START, ALIGN_MIDDLE
+import threading as td
+import time
+
+class SecondThread(td.Thread):
+    def __init__(self, ThisPtr):
+        td.Thread.__init__(self)
+        self.setDaemon(True)
+        self.ThisPtr = ThisPtr
+
+    def run(self):
+        try:
+            while True:
+                self.ThisPtr.queue_draw()
+                time.sleep(1)
+        except Exception, e:
+            print "class SecondThread got error %s" % e
 
 class DateTime(gtk.VBox):
     def __init__(self, 
@@ -46,101 +62,37 @@ class DateTime(gtk.VBox):
         self.width = width
         self.height = height
         self.set_size_request(self.width, self.height)
-        self.text_size = self.height - 60
 
-        '''
-        + button
-        '''
-        self.top_align = self.__setup_align()
-        self.top_box = gtk.HBox(spacing = box_spacing)
-        self.hour_up_button = Button("+")
-        self.hour_up_button.connect("button-press-event", self.__hour_up_button_press)
-        self.minute_up_button = Button("+")
-        self.minute_up_button.connect("button-press-event", self.__minute_up_button_press)
-        self.__widget_pack_start(self.top_box, [self.hour_up_button, self.minute_up_button])
-        self.top_align.add(self.top_box)
-        '''
-        datetime label
-        '''
-        self.datetime_align = self.__setup_align()
-        self.datetime_box = gtk.HBox(spacing=10)
-        self.hour_label = self.__setup_label(("%d" % self.hour_value).zfill(2), self.text_size, self.height)
-        self.comma_label = self.__setup_label(":", self.text_size, 30)
-        self.minute_label = self.__setup_label(("%d" % self.minute_value).zfill(2), self.text_size, self.height)
-        self.__widget_pack_start(self.datetime_box, 
-                                 [self.hour_label, self.comma_label, self.minute_label])
-        self.datetime_align.add(self.datetime_box)
-        '''
-        - button
-        '''
-        self.buttom_align = self.__setup_align()
-        self.buttom_box = gtk.HBox(spacing = box_spacing)
-        self.hour_down_button = Button("-")
-        self.hour_down_button.connect("button-press-event", self.__hour_down_button_press)
-        self.minute_down_button = Button("-")
-        self.minute_down_button.connect("button-press-event", self.__minute_down_button_press)
-        self.__widget_pack_start(self.buttom_box, [self.hour_down_button, self.minute_down_button])
-        self.buttom_align.add(self.buttom_box)
-        '''
-        this->VBox pack_start
-        '''
-        self.__widget_pack_start(self, [self.top_align, self.datetime_align, self.buttom_align])
+        self.clockface = ui_theme.get_pixbuf("datetime/clockface.png")
+        self.hourhand = ui_theme.get_pixbuf("datetime/hourhand.png")
+        self.minhand = ui_theme.get_pixbuf("datetime/minhand.png")
+        self.sechand = ui_theme.get_pixbuf("datetime/sechand.png")
 
-    def __setup_align(self, 
-                      xalign=0.5, 
-                      yalign=0.5, 
-                      xscale=1.0, 
-                      yscale=1.0, 
-                      padding_top=10, 
-                      padding_bottom=10, 
-                      padding_left=10, 
-                      padding_right=10):
-        align = gtk.Alignment()
-        align.set(xalign, yalign, xscale, yscale)
-        align.set_padding(padding_top, padding_bottom, padding_left, padding_right)
-        return align
+        self.connect("expose-event", self.__expose)
 
-    def __setup_label(self, text, text_size=40, width=60, align=ALIGN_MIDDLE):
-        label = Label(text, None, text_size, align, width)
-        return label
-    
-    def __widget_pack_start(self, parent_widget, widgets=[], expand=False, fill=False):
-        if parent_widget == None:                                                
-            return                                                               
-            
-        for item in widgets:                                                     
-            parent_widget.pack_start(item, expand, fill)
+        SecondThread(self).start()
 
-    def __hour_up_button_press(self, widget, event):
-        self.hour_value += 1
+    def __expose(self, widget, event):
+        cr = widget.window.cairo_create()
+        rect = widget.allocation
+        x, y, w, h = rect.x, rect.y, rect.width, rect.height
+        self.hour_value = time.localtime().tm_hour
+        self.minute_value = time.localtime().tm_min
+        self.second_value = time.localtime().tm_sec
 
-        if self.hour_value >= 24:
-            self.hour_value = 0
-
-        self.hour_label.set_text(("%d" % self.hour_value).zfill(2))
-
-    def __hour_down_button_press(self, widget, event):
-        self.hour_value -= 1
-
-        if self.hour_value <= 0:
-            self.hour_value = 23
-
-        self.hour_label.set_text(("%d" % self.hour_value).zfill(2))
-
-    def __minute_up_button_press(self, widget, event):
-        self.minute_value += 1
-
-        if self.minute_value >= 60:
-            self.minute_value = 0
-
-        self.minute_label.set_text(("%d" % self.minute_value).zfill(2))
-
-    def __minute_down_button_press(self, widget, event):
-        self.minute_value -= 1
-
-        if self.minute_value <= 0:
-            self.minute_value = 59
-
-        self.minute_label.set_text(("%d" % self.minute_value).zfill(2))
+        with cairo_state(cr):
+            draw_pixbuf(cr, self.clockface.get_pixbuf(), x, y)
+            draw_pixbuf(cr, 
+                        self.hourhand.get_pixbuf(), 
+                        x + (self.clockface.get_pixbuf().get_width() - self.hourhand.get_pixbuf().get_width()) / 2, 
+                        y)
+            draw_pixbuf(cr, 
+                        self.minhand.get_pixbuf(), 
+                        x + (self.clockface.get_pixbuf().get_width() - self.minhand.get_pixbuf().get_width()) / 2, 
+                        y)
+            draw_pixbuf(cr, 
+                        self.sechand.get_pixbuf(), 
+                        x + (self.clockface.get_pixbuf().get_width() - self.sechand.get_pixbuf().get_width()) / 2, 
+                        y)
 
 gobject.type_register(DateTime)
