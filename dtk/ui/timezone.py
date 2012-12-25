7c62f5dcadc0c1e603f39acde049510fb5a655c1
xiangzhai <xiangzhai83@gmail.com>
2012-12-25 12:05:56 +0800

    Adjust TimeZone widget

--- dtk/ui/timezone.py
+++ dtk/ui/timezone.py
@@ -29,7 +29,7 @@ class TimeZone(gtk.EventBox):
     __gsignals__ = {
         "changed" : (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (int,)),}
     
-    def __init__(self, timezone=0, width=800, height=409):
+    def __init__(self, timezone=0, width=800, height=409, padding_top=0, padding_left=0):
         gtk.EventBox.__init__(self)
         
         self.__timezone = timezone + 9
@@ -37,6 +37,9 @@ class TimeZone(gtk.EventBox):
         self.width = width
         self.height = height
         self.set_size_request(self.width, self.height)
+
+        self.padding_top = padding_top
+        self.padding_left = padding_left
 
         self.__const_width = 800
         self.__const_height = 409
@@ -81,6 +84,8 @@ class TimeZone(gtk.EventBox):
         cr = widget.window.cairo_create()
         rect = widget.allocation
         x, y = rect.x, rect.y
+        x -= self.padding_left
+        y -= self.padding_top
 
         with cairo_state(cr):
             bg_dpixbuf = self.bg_pixbuf.get_pixbuf()
