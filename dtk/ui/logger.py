ea9d3620e62cfbfeafc88946694e82482c7bfca7
Andy Stewart <lazycat.manatee@gmail.com>
2013-01-05 23:26:39 +0700

    Refactory name of module Logger, it's better there have a demo to test Logger. :)

--- dtk/ui/logger.py
+++ dtk/ui/logger.py
@@ -26,22 +26,27 @@ import re
 levelno = logging.INFO
 classfilter = []
 
-def setLevelNo(n):
+def set_level_no(n):
     global levelno
     levelno = ( 100 - (n * 10) )
     
-def setFilter(filter_list):    
+def set_filter(filter_list):    
     global classfilter
     classfilter = filter_list
 
 class MyFilter(logging.Filter):
-    def __init__(self, name=""): pass
+    def __init__(self, name=""): 
+        pass
+    
     def filter(self, record):
-        if record.levelno >= levelno: return True
+        if record.levelno >= levelno: 
+            return True
+        
         for filter in classfilter:
-            if record.name.startswith(filter): return True
+            if record.name.startswith(filter): 
+                return True
+            
         return False
-
 
 logger = logging.getLogger("")
 logger.setLevel(logging.DEBUG)
@@ -59,56 +64,58 @@ def objaddr(obj):
     string = object.__repr__(obj)
     m = re.search("at (0x\w+)",string)
     if m: return  m.group(1)[2:]
+    
     return "       "
 
 class Logger(object):
 
-    def set_logname(self, name):
+    def set_log_name(self, name):
         self.__logname = name
 
-    def get_logname(self):
+    def get_log_name(self):
         if hasattr(self,"__logname") and self.__logname :
             return self.__logname
         else:
             return "%s.%s"%(self.__module__,self.__class__.__name__)
 
-    def logdebug(self, msg, *args, **kwargs): 
+    def log_debug(self, msg, *args, **kwargs): 
         # msg = "%s %s"%(objaddr(self),msg)
-        mylogger = logging.getLogger(self.get_logname())
+        mylogger = logging.getLogger(self.get_log_name())
         mylogger.debug(msg, *args, **kwargs)
 
-    def loginfo(self, msg, *args, **kwargs): 
+    def log_info(self, msg, *args, **kwargs): 
         # msg = "%s  %s"%(objaddr(self),msg)
-        mylogger = logging.getLogger(self.get_logname())
+        mylogger = logging.getLogger(self.get_log_name())
         mylogger.info(msg, *args, **kwargs)
 
-    def logwarn(self, msg, *args, **kwargs): 
+    def log_warn(self, msg, *args, **kwargs): 
         # msg = "%s  %s"%(objaddr(self),msg)
-        mylogger = logging.getLogger(self.get_logname())
+        mylogger = logging.getLogger(self.get_log_name())
         mylogger.warn(msg, *args, **kwargs)
 
-    def logerror(self, msg, *args, **kwargs): 
+    def log_error(self, msg, *args, **kwargs): 
         # msg = "%s  %s"%(objaddr(self),msg)
-        mylogger = logging.getLogger(self.get_logname())
+        mylogger = logging.getLogger(self.get_log_name())
         mylogger.error(msg, *args, **kwargs)
 
-    def logcritical(self, msg, *args, **kwargs): 
+    def log_critical(self, msg, *args, **kwargs): 
         # msg = "%s  %s"%(objaddr(self),msg)
-        mylogger = logging.getLogger(self.get_logname())
+        mylogger = logging.getLogger(self.get_log_name())
         mylogger.critical(msg, *args, **kwargs)
 
-    def logexception(self, msg, *args, **kwargs):
+    def log_exception(self, msg, *args, **kwargs):
         # msg = "%s  %s"%(objaddr(self),msg)
-        mylogger = logging.getLogger(self.get_logname())
+        mylogger = logging.getLogger(self.get_log_name())
         mylogger.exception(msg, *args, **kwargs)
 
-    def logdeprecated(self, msg, *args, **kwargs):
+    def log_deprecated(self, msg, *args, **kwargs):
         # msg = "%s  %s"%(objaddr(self),msg)
-        mylogger = logging.getLogger(self.get_logname())
+        mylogger = logging.getLogger(self.get_log_name())
         mylogger.log(100,msg, *args, **kwargs)
 
-def newLogger(name):
+def new_logger(name):
     l = Logger()
-    l.set_logname(name)
+    l.set_log_name(name)
+    
     return l
     
