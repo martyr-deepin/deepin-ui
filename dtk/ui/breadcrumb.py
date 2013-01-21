ac4d7489d81bcf6689afd9e2b1267417b16a2700
Andy Stewart <lazycat.manatee@gmail.com>
2013-01-21 22:29:16 +0700

    Don't draw bg on breadcrumb.

--- dtk/ui/breadcrumb.py
+++ dtk/ui/breadcrumb.py
@@ -28,8 +28,8 @@ from skin_config import skin_config
 from menu import Menu
 from constant import  DEFAULT_FONT_SIZE
 from draw import (draw_vlinear, draw_line, draw_text, draw_pixbuf)
-from utils import (get_content_size,get_window_shadow_size, cairo_disable_antialias,
-                   color_hex_to_cairo, alpha_color_hex_to_cairo, get_match_parent)
+from utils import (get_content_size, cairo_disable_antialias,
+                   color_hex_to_cairo, alpha_color_hex_to_cairo)
 import gtk 
 import gobject
 import pango
@@ -208,18 +208,9 @@ class Bread(gtk.HBox):
         """
         cr = widget.window.cairo_create()
         rect = widget.allocation
-        pixbuf = ui_theme.get_pixbuf("crumbs_bg.png")
-
-        from dtk.ui.cache_pixbuf import CachePixbuf
-        self.cache_bg_pixbuf = CachePixbuf()
-        self.cache_bg_pixbuf.scale(pixbuf.get_pixbuf(), rect.width, rect.height)
-        draw_pixbuf(cr, self.cache_bg_pixbuf.get_cache(), rect.x, rect.y)
-        #win = get_match_parent(widget, ["ScrolledWindow"])
-        #win_x, win_y = win.allocation.x, win.allocation.y
-
-        #cr.translate(-win_x, -win_y)
-        #(shadow_x, shade_y) = get_window_shadow_size(self.get_toplevel())
-        #skin_config.render_background(cr, widget, shadow_x, shade_y)
+        cr.rectangle(rect.x, rect.y, rect.width, rect.height)
+        cr.clip()
+        skin_config.render_background(cr, widget, 0, 0)
         return False
 
     def add(self, crumbs):
