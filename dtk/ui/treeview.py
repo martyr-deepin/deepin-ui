befb28e8b13f78b60bea18a52736e29441716daa
lovesnow <houshao55@gmail.com>
2013-04-18 14:40:23 +0800

    set treeview sort_methods default value is None

--- dtk/ui/treeview.py
+++ dtk/ui/treeview.py
@@ -390,8 +390,8 @@ class TreeView(gtk.VBox):
                         
                         
     def on_titlebar_clicked_title(self, widget, index, sort_ascending):                    
-        SortThread(lambda : self.sort_column(index, sort_ascending), self.render_sort_column).start()
-                        
+        if self.sort_methods:
+            SortThread(lambda : self.sort_column(index, sort_ascending), self.render_sort_column).start()
         
     def sort_column(self, sort_column_index, sort_ascending):
         # Update sort action id.
@@ -453,7 +453,7 @@ class TreeView(gtk.VBox):
         else:
             print "render_sort_column: drop old sort result!"
         
-    def set_column_titles(self, titles, sort_methods):
+    def set_column_titles(self, titles, sort_methods=None):
         if titles != None and sort_methods != None:
             self.titles = titles
             self.title_box.set_titles(titles)
