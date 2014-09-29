7fb9d7dbf591f7f8284bc9c5864104b3ee252047
宋文武 <songwenwu@linuxdeepin.com>
2014-09-29 10:31:51 +0800

    debian: use pybuild buildsystem, cleanup
      debs:   drop them
      i18n:   use deepin-gettext-tools to generate mo
      demos:  new folder, move releated files into it
      assets: remove unneeded execute permissions
    
    Change-Id: Icddb9c8dfa31cb2e2f9dbd4d4cd7d8ff53f700d5

--- debian/generate_mo.mk
+++
@@ -1,4 +0,0 @@
-# generate locale file 
-
-gen_locale_mo:
-	if [ -d dtk/tools ];then cd dtk/tools && python generate_mo.py; fi
--- setup.py
+++ setup.py
@@ -1,30 +1,28 @@
 #! /usr/bin/env python
 
-from setuptools import setup
 import os
+from setuptools import setup, find_packages
 
-def list_files(target_dir, install_dir):
-    '''List files for option `data_files`.'''
-    results = []
-    for root, dirs, files in os.walk(target_dir):
-        for filepath in files:
-            data_dir = os.path.dirname(os.path.join(root, filepath))
-            data_file = os.path.join(root, filepath)
-            results.append((data_dir, [data_file]))
-            # print results
-    return results                
+def list_datafiles(pkg, dirs, exts):
+    subdirs = []
+    for dir in dirs:
+        subdirs += [ d.lstrip(pkg).lstrip('/') for d, _, _ in os.walk(pkg+'/'+dir) ]
+    files = [ (d+'/'+e) for d in subdirs for e in exts ]
+    return files
 
 setup(name='dtk',
-      version="1.0.3",
-      description='UI toolkit for Linux Deepin.',
-      long_description ="""UI toolkit for Linux Deepin.""",
-      author='Linux Deepin Team',
-      author_email='wangyong@linuxdeepin.com',
-      license='GPL-3',
-      url="https://github.com/linuxdeepin/deepin-ui",
-      download_url="git://github.com/linuxdeepin/deepin-ui.git",
-      platforms = ['Linux'],
-      packages = ['dtk', 'dtk.ui'],
-      data_files = list_files("dtk/theme","dtk/theme") + list_files("dtk/locale", "dtk/locale") + list_files("dtk/skin", "dtk/skin"),
-      )
-
+    version="1.0.3",
+    description='UI toolkit for Linux Deepin.',
+    long_description ="""UI toolkit for Linux Deepin.""",
+    author='Linux Deepin Team',
+    author_email='wangyong@linuxdeepin.com',
+    license='GPL-3',
+    url="https://github.com/linuxdeepin/deepin-ui",
+    download_url="git://github.com/linuxdeepin/deepin-ui.git",
+    platforms = ['Linux'],
+    packages = find_packages(),
+    include_package_data = True,
+    package_data = {
+        '': list_datafiles('dtk', ['theme', 'skin'], ['*.png', '*.jpg', '*.txt', '*.ini'])
+    }
+)
